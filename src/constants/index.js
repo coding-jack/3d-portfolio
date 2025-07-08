68acd86fb093978a4a913fa1b7fb5de57f4fe493
Jackson Kaufman <jacksonkaufman115@gmail.com>
2025-07-08 12:02:46 -0600

    removing React from Graphik experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -181,7 +181,7 @@ const experiences = [
     iconBg: "#383E56",
     date: "July 2017 - May 2019",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Developing and maintaining WordPress websites of various sizes and complexities.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
